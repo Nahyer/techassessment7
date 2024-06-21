cab6e6f73cdc4b499c98388f6883d670d7f857e2
nahyer <reyhanluyai@gmail.com>
2024-06-21 17:04:15 +0300

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import BaseComponent from './components/BaseComponent'
+
 import Todo from './components/Todo'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
